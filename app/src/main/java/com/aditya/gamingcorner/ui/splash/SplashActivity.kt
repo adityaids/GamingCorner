609a4533072e90461ed83e374599d48ce96bd521
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-07-23 21:23:19 +0700

    edit gradle module

--- app/src/main/java/com/aditya/gamingcorner/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/aditya/gamingcorner/ui/splash/SplashActivity.kt
@@ -1,12 +1,20 @@
 package com.aditya.gamingcorner.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.aditya.gamingcorner.R
+import com.aditya.gamingcorner.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 10000L)
     }
 }
