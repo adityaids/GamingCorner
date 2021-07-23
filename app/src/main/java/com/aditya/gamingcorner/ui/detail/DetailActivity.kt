9cf525759677c2c6e0296f324932805e649d21f1
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-07-23 14:39:56 +0700

    Initial commit

---
+++ app/src/main/java/com/aditya/gamingcorner/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.aditya.gamingcorner.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aditya.gamingcorner.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
