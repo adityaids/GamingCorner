e496ba900d6bf2a5b224823db0e1576813a22199
aditya.purnamaaa@gmail.com <aditya.purnamaaa@gmail.com>
2021-07-26 23:47:18 +0700

    added retrofit config

--- core/src/main/java/com/aditya/core/data/source/remote/response/ApiService.kt
+++ core/src/main/java/com/aditya/core/data/source/remote/response/ApiService.kt
@@ -1,15 +1,16 @@
 package com.aditya.core.data.source.remote.response
 
+import io.reactivex.Flowable
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("games?key={key}&search={search}")
-    suspend fun searchGame(
+    fun searchGame(
         @Query("key") key: String,
         @Query("search") search: String
-    ): AutoFillGameListResponse
+    ): Flowable<AutoFillGameListResponse>
 
     @GET("games?key={key}&ordering=released")
     suspend fun latestGame(
